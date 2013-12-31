c769ba09579adeff1dc997d0840ea24694c4fc14
Christopher Broome <ChrisBroome7@gmail.com>
2013-12-31 00:26:42 -0500

    Add ActionBar buttons and handlers

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.myfirstapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -14,6 +19,52 @@ public class MainActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.main_activity_actions, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // handle presses on the action bar items
+    switch(item.getItemId()) {
+      case R.id.action_search:
+        openSearch();
+        return true;
+      case R.id.action_settings:
+        openSettings();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  public void openSearch() {
+    new AlertDialog.Builder(this)
+      .setTitle("Search")
+      .setMessage("Search not implemented")
+      .setNeutralButton("OK", new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialog, int which) {
+          dialog.dismiss();
+        }
+      })
+      .show();
+  }
+
+  public void openSettings() {
+    new AlertDialog.Builder(this)
+      .setTitle("Settings")
+      .setMessage("Settings not implemented")
+      .setNeutralButton("OK", new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialog, int which) {
+          dialog.dismiss();
+        }
+      })
+      .show();
   }
 
   /** Called when the user clicks the Send button. */
