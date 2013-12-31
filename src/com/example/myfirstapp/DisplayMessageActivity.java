cb883f44c43ddaa9c5595778a3b38e4618da218c
Christopher Broome <ChrisBroome7@gmail.com>
2013-12-31 00:36:36 -0500

    Fix app crash when using ActionBar Up button

--- src/com/example/myfirstapp/DisplayMessageActivity.java
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -15,6 +14,7 @@ public class DisplayMessageActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    getActionBar().setDisplayHomeAsUpEnabled(true);
     //setContentView(R.layout.title_activity_display_message);
     //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
       // show the up button in the action bar
@@ -34,11 +34,6 @@ public class DisplayMessageActivity extends Activity {
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    switch(item.getItemId()) {
-      case android.R.id.home:
-        NavUtils.navigateUpFromSameTask(this);
-        return true;
-    }
     return super.onOptionsItemSelected(item);
   }
 }
